4f1375cf6c0fdd99301e963889d0aaf4ebeef7a5
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-17 12:10:33 +0530

    Unlocked the landing page
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Page() {
-  const { user } = useUser();
-
-  useEffect(() => {
-    if (!user) {
-      redirect("/login");
-    } else {
-      redirect("/signup");
-    }
-  }, [user]);
-
-  return <>Loading</>;
+  return <>Landing Page</>;
 }
