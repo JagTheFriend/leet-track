d5a07449553f71ec9cfdcb4a46a23fda630c9986
Eshita Kapat <eshita.kapat@outlook.com>
2025-06-19 20:38:45 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 import HeroSection from "@components/Essentials/HeroSection";
 import LandingNavbar from "@components/Essentials/LandingNavbar";
-
 import Features from "@components/Essentials/Features";
 
 export default function Page() {
   return (
     <>
-
+      <main>
+        <LandingNavbar />
+        <HeroSection />
+      </main>
       <Features />
     </>
   );
-
-      <main>
-
-          <LandingNavbar />
-          <HeroSection />
-
-      </main>
-    </>
-  )
-
 }
