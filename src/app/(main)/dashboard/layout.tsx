d094d49179d1ef8a3e09c38eca147fb98253393b
Eshita Kapat <eshita.kapat@outlook.com>
2025-06-23 12:22:47 +0530

    Loader Spinner Added

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -7,7 +7,13 @@ export default function CalenderLayout({
 }) {
   return (
     <Suspense
-      fallback={<div className="p-8 text-center">Loading reminders...</div>}
+      fallback={
+        <div className="flex justify-center items-center min-h-[80vh] w-full">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#818cf8]">
+
+          </div>
+        </div>
+      }
     >
       {children}
     </Suspense>
