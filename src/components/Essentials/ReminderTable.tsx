ec1749b4929c9d43e6b470f9e802e5e43aaea666
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-24 15:09:37 +0530

    Added buttons to reminder table
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Essentials/ReminderTable.tsx
+++ src/components/Essentials/ReminderTable.tsx
@@ -11,16 +11,10 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Badge } from "@components/ui/badge";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-  ContextMenuTrigger,
-} from "@components/ui/context-menu";
+import { Button } from "@components/ui/button";
 import { cn } from "@lib/utils";
 import { PROBLEM_DIFFICULTY, Reminder, REMINDER_STATUS } from "@prisma-client";
-import { LoaderIcon, SquareArrowOutUpRight } from "lucide-react";
+import { Loader, SquareArrowOutUpRight } from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
@@ -32,60 +26,66 @@ export default function RemainderTable({
 }: {
   reminders: Reminder[];
 }) {
-  return (
-    <div className="border border-[#e5e7eb] dark:border-[#6366f1]  p-4 rounded-lg shadow-md ">
-      <h2 className="text-xl text-gray-800 font-semibold">Your Reminders</h2>
-      <p className="text-sm text-muted-foreground mb-2 ">
-        Manage your scheduled LeetCode problem reminders
-      </p>
-      <Card className="dark:bg-[#d1d5db] text-black ">
-        <CardContent className="p-1">
-          <Table>
-            <TableHeader className="text-base">
-              <TableRow className="border-b border-[#e5e7eb] dark:border-[#6366f1] bg-[#f3f4f6] ">
-                <TableHead className="dark:text-black">Question</TableHead>
-                <TableHead className="dark:text-black">Difficulty</TableHead>
-                <TableHead className="dark:text-black">Scheduled Date</TableHead>
-                <TableHead className="dark:text-black">Status</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody className="text-base cursor-default">
-              {reminders.map((reminder, index) => (
-                <CustomContextMenu
-                  key={index + Math.random()}
-                  reminder={reminder}
-                >
-                  <TableRow
-  className={cn(
-    "cursor-pointer",
-    "dark:border-b dark:border-b-[#6366f1] last:border-b-0", // <-- add this!
-    reminder.reminderStatus === "COMPLETED"
-      ? "opacity-50"
-      : reminder.reminderStatus === "UPCOMING"
-      ? "opacity-75"
-      : ""
-  )}
-  onClick={() => {
-    window.open(
-      `https://leetcode.com/problems/${reminder.problemSlug}`,
-      "_blank"
-    );
-  }}
->
-  <TableRowContent reminder={reminder} />
-</TableRow>
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedReminder, setSelectedReminder] = useState<Reminder>()
 
-                </CustomContextMenu>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    </div>
+  return (
+    <>
+      <div className="border border-[#e5e7eb] dark:border-[#6366f1]  p-4 rounded-lg shadow-md ">
+        <h2 className="text-xl text-gray-800 font-semibold">Your Reminders</h2>
+        <p className="text-sm text-muted-foreground mb-2 ">
+          Manage your scheduled LeetCode problem reminders
+        </p>
+        <Card className="dark:bg-[#d1d5db] text-black ">
+          <CardContent className="p-1">
+            <Table>
+              <TableHeader className="text-base">
+                <TableRow className="border-b border-[#e5e7eb] dark:border-[#6366f1] bg-[#f3f4f6] ">
+                  <TableHead className="dark:text-black">Question</TableHead>
+                  <TableHead className="dark:text-black">Difficulty</TableHead>
+                  <TableHead className="dark:text-black">Scheduled Date</TableHead>
+                  <TableHead className="dark:text-black">Status</TableHead>
+                  <TableHead className="dark:text-black"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody className="text-base cursor-default">
+                {reminders.map((reminder, index) => (
+                  <TableRow
+                    key={index + Math.random()}
+                    className={cn(
+                      "cursor-pointer",
+                      "dark:border-b dark:border-b-[#6366f1] last:border-b-0", // <-- add this!
+                      reminder.reminderStatus === "COMPLETED"
+                        ? "opacity-50"
+                        : reminder.reminderStatus === "UPCOMING"
+                          ? "opacity-75"
+                          : ""
+                    )}
+                  >
+                    <TableRowContent
+                      reminder={reminder}
+                      setSelectedReminder={setSelectedReminder}
+                      setIsDialogOpen={setIsDialogOpen}
+                    />
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+      {selectedReminder && <EditReminderModal
+        reminder={selectedReminder}
+        isDialogOpen={isDialogOpen}
+        setIsDialogOpen={setIsDialogOpen}
+      />
+      }
+    </>
   );
 }
 
-function TableRowContent({ reminder }: { reminder: Reminder }) {
+
+function TableRowContent({ reminder, setSelectedReminder, setIsDialogOpen }: { reminder: Reminder, setSelectedReminder: (v: Reminder) => unknown, setIsDialogOpen: (v: boolean) => unknown }) {
   const getDifficultyBadge = useCallback((level: PROBLEM_DIFFICULTY) => {
     const variant = {
       EASY: "bg-green-100 text-green-600",
@@ -106,8 +106,8 @@ function TableRowContent({ reminder }: { reminder: Reminder }) {
       status === "COMPLETED"
         ? "bg-green-500"
         : status === "PENDING"
-        ? "bg-red-500"
-        : "bg-gray-600";
+          ? "bg-red-500"
+          : "bg-gray-600";
     return (
       <Badge variant={"secondary"} className={cn("text-white", bgStyle)}>
         {status}
@@ -137,18 +137,22 @@ function TableRowContent({ reminder }: { reminder: Reminder }) {
         })}
       </TableCell>
       <TableCell>{getStatusBadge(reminder.reminderStatus)}</TableCell>
+      <TableCell className="flex flex-row gap-2">
+        <Button
+          className="cursor-pointer"
+          size='sm'
+          onClick={() => {
+            setSelectedReminder(reminder)
+            setIsDialogOpen(true)
+          }}
+        >Edit</Button>
+        <DeleteReminderButton reminderId={reminder.id} />
+      </TableCell>
     </>
   );
 }
 
-function CustomContextMenu({
-  children,
-  reminder,
-}: {
-  children: React.ReactNode;
-  reminder: Reminder;
-}) {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+function DeleteReminderButton({ reminderId }: { reminderId: string }) {
   const {
     execute: executeDeleteReminder,
     hasErrored: deleteReminderError,
@@ -166,33 +170,16 @@ function CustomContextMenu({
   }, [deleteReminderError, deleteReminderSuccess]);
 
   return (
-    <>
-      <ContextMenu>
-        <ContextMenuTrigger asChild>{children}</ContextMenuTrigger>
-        <ContextMenuContent className="max-w-fit">
-          <ContextMenuItem inset onClick={() => setIsDialogOpen(true)}>
-            Edit
-          </ContextMenuItem>
-          <ContextMenuSeparator />
-          <ContextMenuItem
-            inset
-            onClick={() => {
-              executeDeleteReminder({ reminderId: reminder.id });
-            }}
-          >
-            {!isDeletingReminder ? (
-              "Delete"
-            ) : (
-              <LoaderIcon className="animate-spin" />
-            )}
-          </ContextMenuItem>
-        </ContextMenuContent>
-      </ContextMenu>
-      <EditReminderModal
-        reminder={reminder}
-        isDialogOpen={isDialogOpen}
-        setIsDialogOpen={setIsDialogOpen}
-      />
-    </>
-  );
+    <Button
+      size='sm'
+      disabled={isDeletingReminder}
+      className="flex flex-row gap-2 cursor-pointer"
+      onClick={() => {
+        executeDeleteReminder({ reminderId })
+      }}
+    >
+      Delete {isDeletingReminder && <Loader className="animate-spin" />}
+    </Button>
+  )
+
 }
